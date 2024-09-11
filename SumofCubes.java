895bfd4800b23f590350db565caee3c4c893ae5e
IanK8 <114188361+IanK8@users.noreply.github.com>
2024-09-10 20:18:10 -0400

    Update SumofCubes.java

--- SumofCubes.java
+++ SumofCubes.java
@@ -1,23 +1,20 @@
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
 
 public class SumofCubes{
     public static void main(String[] args) {
         int num;
-        int sum;
-        sum = 0;
-        List<Integer> list = new ArrayList<>();
+        int integer;
+        int digit;
         Scanner input = new Scanner(System.in);
-        System.out.print("Input a whole number ");
+        System.out.print("Enter a number: ");
         num = input.nextInt();
-        while (num > 0){
-            list.add((num % 10)*(num % 10)*(num % 10));
-            num = num / 10;
-        }
-        for (int i = 0; 1 < list.size(); i++) {
-            sum += list.get(1);
-        }
-        System.out.println(sum);
+        while (num > 0) {
+            digit = num % 10;
+               System.out.print(digit + "*" + digit + "*" + digit + "+");
+               num = num / 10;
+               integer = digit * digit * digit;
+
+            }
+        System.out.print(" = " + digit);
     }
 }
